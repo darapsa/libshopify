69d5f27bff226c10789a41a7ec6a1b36d2052322
ꦌꦫꦶꦏ꧀ꦦꦿꦧꦮꦑꦩꦭ꧀ <erik@darapsa.co.id>
2022-09-20 14:26:36 +0800

    Can't compare iss & dest

--- sessiontoken.h
+++ sessiontoken.h
@@ -12,7 +12,7 @@ static inline bool sessiontoken_isvalid(const char *token, const char *api_key,
 	params.verification_key_length = strlen(api_secret_key);
 	params.validate_exp = 1;
 	params.validate_nbf = 1;
-	params.validate_iss = (char *)shop;
+	//params.validate_iss = (char *)shop;
 	params.validate_aud = (char *)api_key;
 	enum l8w8jwt_validation_result validation;
 	int decode = l8w8jwt_decode(&params, &validation, NULL, NULL);
