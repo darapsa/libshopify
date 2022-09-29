89fdfbac7d827f50c9d2b8e85e739a178367c7db
ꦌꦫꦶꦏ꧀ꦦꦿꦧꦮꦑꦩꦭ꧀ <erik@darapsa.co.id>
2022-09-29 12:51:14 +0800

    Replace break with return now that there's more

--- shopify.c
+++ shopify.c
@@ -504,8 +504,8 @@ static enum MHD_Result handle_request(void *cls, struct MHD_Connection *con,
 						header);
 				MHD_add_response_header(res, "Content-Type",
 						"application/json");
-				ret = MHD_queue_response(con, MHD_HTTP_OK, res);
-				break;
+				return MHD_queue_response(con, MHD_HTTP_OK,
+						res);
 			}
 		i = 0;
 		const struct shopify_carrierservice *carrierservice;
@@ -560,8 +560,8 @@ static enum MHD_Result handle_request(void *cls, struct MHD_Connection *con,
 						header);
 				MHD_add_response_header(res, "Content-Type",
 						"application/json");
-				ret = MHD_queue_response(con, MHD_HTTP_OK, res);
-				break;
+				return MHD_queue_response(con, MHD_HTTP_OK,
+						res);
 			}
 	} else if (session && session->access_token) {
 		if (embedded) {
